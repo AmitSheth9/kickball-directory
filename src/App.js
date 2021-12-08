a147c5fb971fb21b2156e78ddd38c827751727a5
AmitSheth9 <amit.sheth9@gmail.com>
2021-12-07 17:25:34 -0800

    created router page and corresponding components that need to be filled in next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
+import React from 'react';
+import reactDom from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom'; 
+import Home from './Home';
+import Players from './views/Players/Players';
+import PlayerDetails from './views/Players/PlayerDetails';
+import Teams from './views/Teams/Teams';
+import TeamDetails from './views/Teams/TeamDetails';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/teams'>Teams</NavLink>
+          <NavLink to='/players'>Players</NavLink>
+        </header>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/teams'>
+            <Teams />
+          </Route>
+          <Route
+            exact path='/teams/:id' >
+            <TeamDetails />
+          </Route>
+          <Route exact path='/players' >
+            <Players />
+          </Route>
+          <Route exact path='/players/:id' >
+            <PlayerDetails/>
+          </Route>
+        </Switch>
+
+
+      </Router>
     </div>
   );
 }
